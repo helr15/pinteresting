22a77db974e05a8f317e4d6b5ec977dc3656adc9
Helen Resor <hresor@gmail.com>
2014-01-23 11:28:15 -0800

    added users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,10 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+
+  # Required for Heroku
+  # Note to set to your actual host
+  config.action_mailer.default_url_options = { :host => 'teamsiekezor.herokuapp.com' }
+
 end
